b7480d38422d787db7af41c7e406f370bf7f5def
andrewmercooloff <am654@yandex.ru>
2025-10-18 00:54:07 +0300

    Revert "Fix puck dragging interference - exclude dragged pucks from collision detection"
    This reverts commit 10386757caa720f0d152f3540fc1145d5f679e32.

--- app/index.tsx
+++ app/index.tsx
@@ -235,9 +235,8 @@ const usePuckCollisionSystem = (players: Player[]) => {
           }
 
           // Жесткая система коллизий - шайбы не могут накладываться
-          // ИСКЛЮЧАЕМ перетаскиваемые шайбы из проверки коллизий
           currentPositions.forEach((otherPos, otherIndex) => {
-            if (otherPos.id === pos.id || otherPos.isDragging) return;
+            if (otherPos.id === pos.id) return;
             
             const dx = newX - otherPos.x;
             const dy = newY - otherPos.y;
