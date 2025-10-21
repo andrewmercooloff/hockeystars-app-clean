5469cf63a7090ab61a7be852c565bb4c822d9c21
andrewmercooloff <am654@yandex.ru>
2025-10-21 15:07:25 +0300

    Fine-tune content position - paddingTop 42px

--- app/exercise-details.tsx
+++ app/exercise-details.tsx
@@ -438,7 +438,7 @@ const styles = StyleSheet.create({
   },
   scrollView: {
     flex: 1,
-    paddingTop: 43, // Отступ для абсолютного заголовка (уменьшен еще на 10px)
+    paddingTop: 42, // Отступ для абсолютного заголовка
   },
   header: {
     position: 'absolute',
